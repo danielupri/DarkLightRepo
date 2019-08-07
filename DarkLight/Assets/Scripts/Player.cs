765458b9ddfb30b176e35b4cd0abf1184316e977
Daniel Uprimny Arcos <danielupri@hotmail.com>
2019-08-08 01:14:46 +0200

    - player movment added (vertical movement, jump, lock jump & wall jump)

--- DarkLight/Assets/Scripts/Player.cs
+++ DarkLight/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Vector3 moveVector;
+    Vector3 lastMotion;
     CharacterController controller;
     float inputDirection;
 
@@ -12,6 +13,7 @@ public class Player : MonoBehaviour
     float verticalVelocity;
     float gravity = 1;
     public float jumpForce = 10;
+    public float isGroundedBuffer = 0.1f;
     
 
     //horizontal movement variables
@@ -26,30 +28,69 @@ public class Player : MonoBehaviour
     }
 
     void FixedUpdate()
-    {
-        print(controller.isGrounded);
+    {      
+        
+
         //horizontal movement
+        moveVector = Vector3.zero;
         inputDirection = Input.GetAxis("Horizontal") * speed;
 
         //vertical movement
-        if (controller.isGrounded)
+        if (IsControllerGrounded())
         {
-            verticalVelocity = 0;
-                
-            if (Input.GetAxis("Jump")>0)
-            {
-                verticalVelocity = jumpForce;
-            }
+            verticalVelocity = Input.GetAxis("Jump")*jumpForce;
+            moveVector.x = inputDirection;
+
         }
         else
         {
             verticalVelocity -= gravity;
+            moveVector.x = lastMotion.x;
         }
-        
-        moveVector = new Vector3(inputDirection, verticalVelocity, 0);
-        controller.Move(moveVector * Time.fixedDeltaTime);         
+
+        moveVector.y = verticalVelocity;
+        controller.Move(moveVector * Time.fixedDeltaTime);
+        lastMotion = moveVector;
         
     }
 
-     
+    private bool IsControllerGrounded()
+    {
+        Vector3 leftRayStart;
+        Vector3 rightRayStart;
+
+        leftRayStart = controller.bounds.center;
+        rightRayStart = controller.bounds.center;
+
+        leftRayStart.x -= controller.bounds.extents.x;
+        leftRayStart.x += controller.bounds.extents.x;
+
+        Debug.DrawRay(leftRayStart, Vector3.down, Color.red);
+        Debug.DrawRay(rightRayStart, Vector3.down, Color.green);
+
+        if (Physics.Raycast(leftRayStart, Vector3.down, (controller.height) / 2 + isGroundedBuffer))
+            return true;
+
+        if (Physics.Raycast(rightRayStart, Vector3.down, (controller.height) / 2 + isGroundedBuffer))
+            return true;
+
+        return false;
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+
+        //wall jump
+        if (controller.collisionFlags == CollisionFlags.Sides && !IsControllerGrounded())
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                Debug.DrawRay(hit.point, hit.normal, Color.red, 2f);
+                moveVector = hit.normal * speed;
+                verticalVelocity = jumpForce;
+            }
+        }
+    }
+
+
 }
