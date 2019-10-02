b10885602ad54b7d2efdfe547ac3b0efb03b523f
Daniel Uprimny Arcos <danielupri@hotmail.com>
2019-10-02 23:25:58 +0200

    Improved player movement

---
+++ DarkLight/Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 10;
+    public float jumpForce = 10;
+    [HideInInspector]
+    public CharacterController controller;
+
+    public float gravityScale = 10;
+
+    private Vector3 moveDirection;
+  
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+
+        print(controller.isGrounded);
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical")*moveSpeed);
+
+        if (controller.isGrounded)
+        {
+            moveDirection.y = 0;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+        else
+        {
+            moveDirection.y += (Physics.gravity.y * gravityScale * Time.deltaTime);
+        }
+        
+        controller.Move(moveDirection * Time.deltaTime);
+        
+
+    }
+}
