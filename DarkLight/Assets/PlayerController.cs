26240725005a489a88f1754cca5e921df137e4fe
Daniel Uprimny Arcos <danielupri@hotmail.com>
2019-10-24 23:27:02 +0200

    - player controller input added

--- DarkLight/Assets/PlayerController.cs
+++ DarkLight/Assets/PlayerController.cs
@@ -5,41 +5,116 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float moveSpeed = 10;
-    public float jumpForce = 10;
-    [HideInInspector]
-    public CharacterController controller;
-
-    public float gravityScale = 10;
-
-    private Vector3 moveDirection;
-  
-    void Start()
+    [System.Serializable]
+    public class MoveSettings
     {
-        controller = GetComponent<CharacterController>();
+        public float forwardVel = 12;
+        public float rotateVel = 100;
+        public float jumpVel = 25;
+        public float distToGround = 0.1f;
+        public LayerMask groundMask;
     }
 
-    void Update()
+    [System.Serializable]
+    public class PhysSettings
     {
+        public float gravity = 0.75f;
+    }
 
-        print(controller.isGrounded);
-        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical")*moveSpeed);
+    [System.Serializable]
+    public class InputSettings
+    {
+        public float inputDelay = 0.1f;
+        public string FORWARD_AXIS = "Vertical";
+        public string TURN_AXIX = "Horizontal";
+        public string JUMP_AXIX = "Jump";
 
-        if (controller.isGrounded)
+    }
+
+    public MoveSettings moveSettings = new MoveSettings();
+    public PhysSettings physSettings = new PhysSettings();
+    public InputSettings inputSettings = new InputSettings();
+
+    Vector3 velocity = Vector3.zero;
+    Quaternion targetRotation;
+    Rigidbody rb;
+    float forwardInput, turnInput, jumpInput;
+
+    public Quaternion TargetRotation
+    {
+        get { return targetRotation; }
+    }
+
+    bool Grounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, moveSettings.groundMask);
+    }
+
+    private void Start()
+    {
+        targetRotation = transform.rotation;
+        rb = GetComponent<Rigidbody>();
+        forwardInput = turnInput = jumpInput = 0;
+    }
+
+    private void Update()
+    {
+        GetInput();
+        Turn();
+    }
+
+    private void FixedUpdate()
+    {
+        Run();
+        Jump();
+
+        rb.velocity = transform.TransformDirection(velocity);
+    }
+
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis(inputSettings.FORWARD_AXIS);
+        turnInput = Input.GetAxis(inputSettings.TURN_AXIX);
+        jumpInput = Input.GetAxisRaw(inputSettings.JUMP_AXIX);
+
+    }
+
+    void Run()
+    {
+        if (Mathf.Abs(forwardInput) > inputSettings.inputDelay)
         {
-            moveDirection.y = 0;
-            if (Input.GetButtonDown("Jump"))
-            {
-                moveDirection.y = jumpForce;
-            }
+            velocity.z = moveSettings.forwardVel * forwardInput;
         }
         else
         {
-            moveDirection.y += (Physics.gravity.y * gravityScale * Time.deltaTime);
+            velocity.z = 0;
         }
-        
-        controller.Move(moveDirection * Time.deltaTime);
-        
+    }
 
+    void Turn()
+    {
+        if (Mathf.Abs(turnInput) > inputSettings.inputDelay)
+        {
+            targetRotation *= Quaternion.AngleAxis(moveSettings.rotateVel * turnInput * Time.deltaTime, Vector3.up);
+        }
+        transform.rotation = targetRotation;
+    }
+
+    void Jump()
+    {
+        if(jumpInput>0 && Grounded())
+        {
+            velocity.y = moveSettings.jumpVel;
+        }
+
+        else if (jumpInput==0 && Grounded())
+        {
+            velocity.y = 0;
+        }
+
+        else
+        {
+            velocity.y -= physSettings.gravity;
+        }
     }
 }
