10ca604a2f4da066eaaf1af85b733c902d396529
Daniel Uprimny Arcos <danielupri@hotmail.com>
2019-11-20 23:03:34 +0100

    - character controller update
    Test script deleted

---
+++ DarkLight/Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+
+    public float graivityModifier = 1f;
+
+    protected Rigidbody rb;
+    protected Vector3 velocity;
+
+    protected const float minMoveDistance = 0.001f;
+
+    private void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        velocity += graivityModifier * Physics.gravity * Time.deltaTime;
+
+        Vector3 deltaPosition = velocity * Time.deltaTime;
+
+        Vector3 move = Vector3.up*deltaPosition.y;
+
+        Movement(move);
+        
+    }
+
+    void Movement(Vector3 move)
+    {
+        float distance = move.magnitude;
+        
+        if (distance > minMoveDistance)
+        {
+           
+        }
+
+        rb.position = rb.position + move;
+    }
+}
